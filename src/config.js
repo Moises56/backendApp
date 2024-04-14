5eac18fdd7201437aa54bb6580fe70c4f51cc951
moises0814 <moises.aviles@amdc.hn>
2024-04-14 13:43:01 -0600

    first commit

--- src/config.js
+++ src/config.js
@@ -1,8 +1,14 @@
 import dotenv from "dotenv";
 dotenv.config();
+console.log(process.env.MYSQL_HOST);
+console.log(process.env.MYSQL_USER);
+console.log(process.env.MYSQL_PASSWORD);
+console.log(process.env.MYSQL_DATABASE);
+
 
 export const pool = {
     host: process.env.MYSQL_HOST || 'localhost',
+    port: process.env.MYSQL_PORT || 3306,
     user: process.env.MYSQL_USER || 'root',
     password: process.env.MYSQL_PASSWORD || '',
     database: process.env.MYSQL_DATABASE || 'tasksdb'
